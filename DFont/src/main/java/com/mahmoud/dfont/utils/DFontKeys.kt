fd289933d0209488b98545d2d56d94d783d1dbe5
Mahmoud Abdallah <mabdallah863@gmail.com>
2021-12-18 18:22:15 +0200

    Add DFont shared preferences object. Add unit tests for DFont sharedPrefs. Add testing dependencies

--- DFont/src/main/java/com/mahmoud/dfont/utils/DFontKeys.kt
+++ DFont/src/main/java/com/mahmoud/dfont/utils/DFontKeys.kt
@@ -7,13 +7,12 @@ object DFontKeys {
 
     val DFONT_SHARED_PREFERENCES_NAME = "dfont_sharedpreferences_name"
 
-
-    // typeface
-    val DFONT_TYPEFACE = "dfont_typeface"
-
-
     // Shared Preferences Default values
     val STRING_DEFAULT_VALUE = ""
     val INT_DEFAULT_VALUE = Integer.MIN_VALUE
+
+    // typeface
+    val DFONT_TYPEFACE = "dfont_typeface"
     val FONT_DEFAULT_VALUE = ResourcesCompat.ID_NULL
+
 }
