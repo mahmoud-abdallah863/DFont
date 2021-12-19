28e0e809eba3c521ffa89c9c85f49ad6ab647338
Mahmoud Abdallah <mabdallah863@gmail.com>
2021-12-19 16:12:43 +0200

    Add get/set int in SFontsharedPrefs + there test functions. Add clear function in DFontSharedPrefs

--- DFont/src/main/java/com/mahmoud/dfont/utils/DFontKeys.kt
+++ DFont/src/main/java/com/mahmoud/dfont/utils/DFontKeys.kt
@@ -3,4 +3,8 @@ package com.mahmoud.dfont.utils
 object DFontKeys {
 
     val DFONT_SHARED_PREFERENCES_NAME = "dfont_sharedpreferences_name"
+
+    // Default values
+    val STRING_DEFAULT_VALUE = ""
+    val INT_DEFAULT_VALUE = Integer.MIN_VALUE
 }
